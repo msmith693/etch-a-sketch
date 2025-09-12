8eb1b4e2d2d7687f8d804724c8ed1068d7825110
msmith693 <198065835+msmith693@users.noreply.github.com>
2025-09-12 20:11:41 +0100

    Add increasing opacity and random color logic
    Random color was generated using a function that randomly generated rgb values.
    Increasing opacity done by incrementing opacity and resetting the updated attribute each time square was entered.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 const container = document.querySelector(".container");
+const generateNewGridBtn = document.querySelector("#generator");
 
 function generateStartGrid(){
   for(let i=0; i<=255; i++){
     let square = document.createElement("div");
     square.classList.add("square");
-    square.setAttribute("style", "width: 60px; height: 60px; border: 2px solid black; box-sizing: border-box;");
+    square.setAttribute("style", "width: 60px; height: 60px; border: 1px solid black; box-sizing: border-box; opacity:0;");
     container.appendChild(square);
+
     square.addEventListener("mouseenter", ()=>{
-      square.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
-  })
-}
+      if(square.style.opacity<=0.9){
+          square.style.opacity = +square.style.opacity+0.1;
+      }
+      square.setAttribute("style", "width: 60px; height: 60px; border: 1px solid black; box-sizing: border-box; background-color: rgb("+generateRandColor()+"); opacity:"+square.style.opacity+";");    })
+  }
 }
 
 generateStartGrid();
 
-const generateNewGridBtn = document.querySelector("#generator");
+
 generateNewGridBtn.addEventListener("click", ()=>{
   let numSquaresPerSide = prompt("How many squares would you like on each side of the grid?");
   if(numSquaresPerSide>=1 && numSquaresPerSide<=100){
@@ -23,11 +27,28 @@ generateNewGridBtn.addEventListener("click", ()=>{
       let squareWidth = 960/numSquaresPerSide;
       let square = document.createElement("div");
       square.classList.add("square");
-      square.setAttribute("style", "width:"+squareWidth+"px; height:"+squareWidth+"px; border: 2px solid black; box-sizing: border-box;");
+      square.setAttribute("style", "width:"+squareWidth+"px; height:"+squareWidth+"px; border: 1px solid black; box-sizing: border-box; opacity:0;");
       container.appendChild(square);
+
       square.addEventListener("mouseenter", ()=>{
-      square.setAttribute("style", "width:"+squareWidth+"px; height:"+squareWidth+"px; border: 2px solid black; box-sizing: border-box; background-color: black;");
+        if(square.style.opacity<=0.9){
+          square.style.opacity = +square.style.opacity+0.1;
+        }
+        square.setAttribute("style", "width:"+squareWidth+"px; height:"+squareWidth+"px; border: 1px solid black; box-sizing: border-box; background-color: rgb("+generateRandColor()+"); opacity:"+square.style.opacity+";");
       })
     }
   }
+  else if(numSquaresPerSide<1){
+    alert("The number of squares per side has to be at least 1.");
+  }
+  else if(numSquaresPerSide>100){
+    alert("There can only be a maximum of 100 squares per side.");
+  }
 })
+
+function generateRandColor(){
+  let redCode = Math.floor(Math.random()*255);
+  let greenCode = Math.floor(Math.random()*255);
+  let blueCode = Math.floor(Math.random()*255);
+  return `${redCode}, ${greenCode}, ${blueCode}`;
+}
