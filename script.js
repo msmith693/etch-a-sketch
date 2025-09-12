293840453a1a95d34910f9277a5d9e16dd3eec89
msmith693 <198065835+msmith693@users.noreply.github.com>
2025-09-12 13:28:57 +0100

    Add highlight on hover functionality
    Was adding event listeners to square divs before appending them to the container div. Need to append first, then add listeners. For each loop was not working. Could not use method getElementById on the squares as they were all identical and so had to add event listeners as they were created, in each iteration of the for loop.

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 const container = document.querySelector(".container");
 
+// let test = document.createElement("div");
+// test.setAttribute("style", "width: 60px; height: 60px; background-color:black;");
+// test.id = "test";
+// container.appendChild(test);
 
+// // const test2 = document.getElementById("test")
+
+// test.addEventListener("mouseenter", (event)=>{
+//   event.target.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
+// })
 
 for(let i=0; i<=255; i++){
   let square = document.createElement("div");
   square.classList.add("square");
   square.setAttribute("style", "width: 60px; height: 60px; border: 2px solid black; box-sizing: border-box;");
   container.appendChild(square);
+  square.addEventListener("mouseenter", ()=>{
+    square.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
+})
 }
+
+// const squares = document.querySelectorAll("square");
+// for(const square of squares){
+//   square.addEventListener("mouseenter", (event)=>{
+//     event.target.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
+//   })
+// }
+// squares.forEach(()=>{
+//   square.addEventListener("mouseenter", (event)=>{
+//     event.target.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
+//     // addHighlight(square);
+//   })
+
+// })
