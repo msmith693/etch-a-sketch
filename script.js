a13c604d13b97d4678d1942b00dca8c70791245c
msmith693 <198065835+msmith693@users.noreply.github.com>
2025-09-12 15:16:36 +0100

    Add generate new grid logic
    Added event listener to button that removes all child nodes of container div when clicked, and adds new divs in accordance with user input

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 const container = document.querySelector(".container");
 
-// let test = document.createElement("div");
-// test.setAttribute("style", "width: 60px; height: 60px; background-color:black;");
-// test.id = "test";
-// container.appendChild(test);
-
-// // const test2 = document.getElementById("test")
-
-// test.addEventListener("mouseenter", (event)=>{
-//   event.target.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
-// })
-
-for(let i=0; i<=255; i++){
-  let square = document.createElement("div");
-  square.classList.add("square");
-  square.setAttribute("style", "width: 60px; height: 60px; border: 2px solid black; box-sizing: border-box;");
-  container.appendChild(square);
-  square.addEventListener("mouseenter", ()=>{
-    square.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
-})
+function generateStartGrid(){
+  for(let i=0; i<=255; i++){
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.setAttribute("style", "width: 60px; height: 60px; border: 2px solid black; box-sizing: border-box;");
+    container.appendChild(square);
+    square.addEventListener("mouseenter", ()=>{
+      square.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
+  })
+}
 }
 
-// const squares = document.querySelectorAll("square");
-// for(const square of squares){
-//   square.addEventListener("mouseenter", (event)=>{
-//     event.target.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
-//   })
-// }
-// squares.forEach(()=>{
-//   square.addEventListener("mouseenter", (event)=>{
-//     event.target.setAttribute("style", "width: 60px; height: 60px; background-color:yellow;");
-//     // addHighlight(square);
-//   })
+generateStartGrid();
 
-// })
+const generateNewGridBtn = document.querySelector("#generator");
+generateNewGridBtn.addEventListener("click", ()=>{
+  let numSquaresPerSide = prompt("How many squares would you like on each side of the grid?");
+  if(numSquaresPerSide>=1 && numSquaresPerSide<=100){
+    container.replaceChildren();  //removes all child nodes
+    for(let i=0; i<numSquaresPerSide**2; i++){
+      let squareWidth = 960/numSquaresPerSide;
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("style", "width:"+squareWidth+"px; height:"+squareWidth+"px; border: 2px solid black; box-sizing: border-box;");
+      container.appendChild(square);
+      square.addEventListener("mouseenter", ()=>{
+      square.setAttribute("style", "width:"+squareWidth+"px; height:"+squareWidth+"px; border: 2px solid black; box-sizing: border-box; background-color: black;");
+      })
+    }
+  }
+})
